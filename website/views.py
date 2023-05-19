ddbbe7d478a61dbc0ef7c8d72b5d62f8fce1f573
= <=>
2023-05-19 16:29:00 +0800

    Add flask_login and @login_required to home page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required,logout_user, current_user
 
 # setup file as Blueprint
 views = Blueprint('views', __name__)
@@ -6,6 +7,7 @@ views = Blueprint('views', __name__)
 # this is where we store out routes
 
 @views.route('/')
+@login_required
 def home():
     return render_template("home.html")
 
