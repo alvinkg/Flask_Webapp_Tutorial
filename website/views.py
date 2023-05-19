7e7f502a0c2b13e2a5a92121562ad7c3c8caf6c8
= <=>
2023-05-19 20:53:16 +0800

    add & delete notes

--- website/views.py
+++ website/views.py
@@ -1,13 +1,42 @@
-from flask import Blueprint, render_template
-from flask_login import login_user, login_required,logout_user, current_user
+from flask import Blueprint,json, jsonify, render_template, request, flash
+from flask_login import login_required,current_user
+from . import db
+from .models import Note
 
 # setup file as Blueprint
 views = Blueprint('views', __name__)
 
 # this is where we store out routes
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        
+        # not sure why so long
+        if len(note) <= 8:
+            # print(len(note))
+            flash('Note empty.', category='error')
+        else:
+            # print('note')
+            # print(len(note))
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added', category='success')
     return render_template("home.html", user=current_user)
 
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # take data from POST request
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
+            
